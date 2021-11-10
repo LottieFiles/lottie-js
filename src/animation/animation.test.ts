160e087387de2a6a6749c90b429469feb3bbacc4
thoaif <thoaif@lottiefiles.com>
2021-11-10 16:30:22 +0500

    test(animation): fixed unique color test

--- src/animation/animation.test.ts
+++ src/animation/animation.test.ts
@@ -1,33 +1,32 @@
-// import clipboardy from 'clipboardy';
 import fetch from 'cross-fetch';
 
-// import { inspect } from 'util';
+import { useRegistry } from '../utils/use-registry';
 import { Animation } from './animation';
 
-function sortObjectKeys(object: Record<string, any>) {
-  return Object.keys(object)
-    .sort()
-    .reduce((accumulator: Record<string, any>, key: string) => {
-      if (Array.isArray(object[key])) {
-        accumulator[key] = object[key].map(sortObjectKeys);
-      }
-      if (typeof object[key] === 'object') {
-        accumulator[key] = sortObjectKeys(object[key]);
-      } else {
-        accumulator[key] = object[key];
-      }
-      return accumulator;
-    }, {});
+function sortObjectKeys(value: any): Record<string, any> {
+  if (Array.isArray(value)) {
+    const newValues = value.map(v => sortObjectKeys(v));
+    newValues.sort();
+    return newValues;
+  } else if (value && typeof value === 'object') {
+    const objectKeys = Object.keys(value);
+    objectKeys.sort();
+    return Object.fromEntries(objectKeys.map(key => [key, sortObjectKeys(value[key])]));
+  } else {
+    return value;
+  }
 }
+
+beforeEach(() => {
+  const registry = useRegistry();
+  registry.clear();
+});
 
 test('Load an animation', async () => {
   const result = await fetch('https://assets1.lottiefiles.com/packages/lf20_SUdZc0.json');
   const json = await result.json();
 
   const anim = await Animation.fromURL('https://assets1.lottiefiles.com/packages/lf20_SUdZc0.json');
-
-  // console.log(inspect(anim.layers[0], false, 20, true));
-  // await clipboardy.write(JSON.stringify(anim));
 
   const sortedJson = sortObjectKeys(json);
   const sortedAnim = sortObjectKeys(JSON.parse(JSON.stringify(anim)));
@@ -36,7 +35,7 @@ test('Load an animation', async () => {
    * Sort the converted Lottie and the original lottie objects by key to make it easier to do a
    * simple string comparison.
    */
-  expect(sortedAnim).toEqual(sortedJson);
+  expect(sortedAnim).toStrictEqual(sortedJson);
 });
 
 test('Get unique colors', async () => {
