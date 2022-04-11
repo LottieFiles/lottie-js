bed13bc194ede316d15ee38c1544965fe541ef58
Wibblist <a.ynew.a@gmail.com>
2022-04-11 15:20:51 +0500

    fix: fixed TextShape enum

--- src/constants/text-shape.ts
+++ src/constants/text-shape.ts
@@ -1,8 +1,8 @@
 export enum TextShape {
-  SQUARE,
-  RAMP_UP,
-  RAMP_DOWN,
-  TRIANGLE,
-  ROUND,
-  SMOOTH,
+  SQUARE = 1,
+  RAMP_UP = 2,
+  RAMP_DOWN = 3,
+  TRIANGLE = 4,
+  ROUND = 5,
+  SMOOTH = 6,
 }
