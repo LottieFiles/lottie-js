dfe51ed752b925324eac287691d540671c12055a
errnull <errnull@@libsvg.com>
2024-04-08 00:41:11 +0800

    feat: 完成资源导出。

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const zlib = require('zlib');
-const { Parser } = require('@lottiefiles/lottie-js');
+const { Parser, Animation } = require('@lottiefiles/lottie-js');
 
 async function run() {
 
@@ -12,30 +12,30 @@ async function run() {
   const svga = parser.load(inflatedData);
   console.log(svga);
 
-  // const anim = await Animation.fromURL('https://assets1.lottiefiles.com/packages/lf20_u4j3xm6r.json');
+  const anim = await Animation.fromURL('https://assets1.lottiefiles.com/packages/lf20_u4j3xm6r.json');
 
-  // // Print some data of the animation
-  // console.log('Frame Rate', anim.frameRate);
-  // console.log('Number of Layers', anim.layers.length);
-  // console.log('Unique Colors', anim.colors);
-  // console.log('Total Frames', anim.totalFrames);
-  // console.log('Durations', anim.duration);
+  // Print some data of the animation
+  console.log('Frame Rate', anim.frameRate);
+  console.log('Number of Layers', anim.layers.length);
+  console.log('Unique Colors', anim.colors);
+  console.log('Total Frames', anim.totalFrames);
+  console.log('Durations', anim.duration);
 
-  // // Manipulate animation
-  // anim.name = 'Woohoo';
-  // anim.width = 512;
-  // anim.height = 512;
+  // Manipulate animation
+  anim.name = svga.name;
+  anim.width = svga.width;
+  anim.height = svga.height;
 
-  // // Get the new JSON
-  // const woohooLottie = JSON.stringify(anim);
-  // // Write JSON data to a local file
-  // fs.writeFile('res/woohooLottie.json', woohooLottie, err => {
-  //   if (err) {
-  //     console.error('Error writing JSON to file:', err);
-  //     return;
-  //   }
-  //   console.log('JSON data has been written to woohooLottie.json');
-  // });
+  // Get the new JSON
+  const svga2lottie = JSON.stringify(anim);
+  // Write JSON data to a local file
+  fs.writeFile('res/svga2lottie.json', svga2lottie, err => {
+    if (err) {
+      console.error('Error writing JSON to file:', err);
+      return;
+    }
+    console.log('JSON data has been written to svga2lottie.json');
+  });
 }
 
 Promise.resolve(run());
